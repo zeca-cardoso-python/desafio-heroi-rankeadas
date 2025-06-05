15c0c0fbaf3211adc419696c2a0aec19079bb479
zeca-cardoso-python <gestao.ticorporativo@gmail.com>
2025-06-04 23:54:55 -0300

    script.js alterado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+function calcularRank(vitorias, derrotas) {
+  const saldoVitorias = vitorias - derrotas;
+  let nivel = "";
+
+  if (vitorias < 10) {
+    nivel = "Ferro";
+  } else if (vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+
+  return { saldoVitorias, nivel };
+}
+
 document
   .getElementById("formHeroi")
   .addEventListener("submit", function (event) {
@@ -7,25 +30,8 @@ document
     const vitorias = parseInt(document.getElementById("vitorias").value);
     const derrotas = parseInt(document.getElementById("derrotas").value);
 
-    const saldoVitorias = vitorias - derrotas;
-    let nivel = "";
+    const resultadoRank = calcularRank(vitorias, derrotas);
+    const resultado = `O Herói ${nome} tem saldo de ${resultadoRank.saldoVitorias} e está no nível de ${resultadoRank.nivel}.`;
 
-    if (vitorias < 10) {
-      nivel = "Ferro";
-    } else if (vitorias <= 20) {
-      nivel = "Bronze";
-    } else if (vitorias <= 50) {
-      nivel = "Prata";
-    } else if (vitorias <= 80) {
-      nivel = "Ouro";
-    } else if (vitorias <= 90) {
-      nivel = "Diamante";
-    } else if (vitorias <= 100) {
-      nivel = "Lendário";
-    } else {
-      nivel = "Imortal";
-    }
-
-    const resultado = `O Herói ${nome} tem saldo de ${saldoVitorias} e está no nível de ${nivel}.`;
     document.getElementById("resultado").textContent = resultado;
   });
